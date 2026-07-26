246e819e14e47e603607d1b635ea431b81b113c0
Meet Ramani <meetramani21@gmail.com>
2026-07-26 18:07:46 +0530

    initialize Vite React TS with Tailwind CSS v4 (@tailwindcss/vite), Recharts, and Axios

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className='w-full h-screen flex items-center justify-center text-2xl'>
+      Welcome To QuerySense-AI Frontend✨
+    </div>
+  )
+}
+
+export default App
